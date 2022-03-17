6173c35dcf1a8a822a704e93f08b6c3e40de46d9
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2022-03-17 23:40:06 +0200

    Refactoring + variant solving

--- src/main/java/epi/arrays/multidimensional/_6/_18/MatrixRotation.java
+++ src/main/java/epi/arrays/multidimensional/_6/_18/MatrixRotation.java
@@ -5,37 +5,70 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-/*
-Write a function that takes as input an n X n 2D array, and rotates the array by
-90 degrees clockwise.
-Time Complexity: O(n^2)
-Space Complexity: O(1)
+/**
+ * Question:
+ * Write a function that takes as input an n X n 2D array,
+ * and rotates the array by 90 degrees clockwise.
+ * ---
+ * Solution:
+ * View matrix as nested layers. The number of layers should be size of matrix / 2.
+ * For each layer, perform rotation of elements.
+ * E.g.
+ *     ┌           ┐
+ *     │ 1   2   3 │
+ *     │           │
+ * A = │ 8   9   4 │
+ *     │           │
+ *     │ 7   6   5 │
+ *     └           ┘
+ * Matrix A has the first layer formed by elements: 1, 2, 3, 4, 5, 6, 7, 8
+ * The first performed rotation is: 1 -> 3 -> 5 -> 7 -> 1.
+ * In a similar fashion we rotate the next elements.
+ * ---
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(1)
  */
 public class MatrixRotation {
 
     public static void main(String[] args) {
-        List<List<Integer>> squareMatrix = Arrays.asList(
-                Arrays.asList(1, 2, 3),
-                Arrays.asList(4, 5, 6),
-                Arrays.asList(7, 8, 9)
-        );
-        rotateMatrix(squareMatrix);
         assertEquals(Arrays.asList(
                 Arrays.asList(7, 4, 1),
                 Arrays.asList(8, 5, 2),
                 Arrays.asList(9, 6, 3)
-        ), squareMatrix);
+        ), rotateMatrix(Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(4, 5, 6),
+                Arrays.asList(7, 8, 9)
+        )));
+
+        assertEquals(Arrays.asList(
+                Arrays.asList(21, 16, 11, 6, 1),
+                Arrays.asList(22, 17, 12, 7, 2),
+                Arrays.asList(23, 18, 13, 8, 3),
+                Arrays.asList(24, 19, 14, 9, 4),
+                Arrays.asList(25, 20, 15, 10, 5)
+        ), rotateMatrix(Arrays.asList(
+                Arrays.asList(1, 2, 3, 4, 5),
+                Arrays.asList(6, 7, 8, 9, 10),
+                Arrays.asList(11, 12, 13, 14, 15),
+                Arrays.asList(16, 17, 18, 19, 20),
+                Arrays.asList(21, 22, 23, 24, 25)
+        )));
     }
 
-    private static void rotateMatrix(List<List<Integer>> squareMatrix) {
+    private static List<List<Integer>> rotateMatrix(List<List<Integer>> squareMatrix) {
         int size = squareMatrix.size();
-        for (int i = 0; i < size / 2; i++) {
+        int layers = size / 2;
+        for (int i = 0; i < layers; i++) {
+            // For each layer, rotate each element
             rotateMatrix(squareMatrix, i, size - 1 - i);
         }
+        return squareMatrix;
     }
 
     private static void rotateMatrix(List<List<Integer>> squareMatrix, int start, int end) {
         for (int i = 0; i < end - start; i++) {
+            // Compute positions on each side of the square matrix
             int temp = squareMatrix.get(end - i).get(start);
             squareMatrix.get(end - i).set(start, squareMatrix.get(end).get(end - i));
             squareMatrix.get(end).set(end - i, squareMatrix.get(start + i).get(end));
--- src/main/java/epi/arrays/multidimensional/_6/_18/variant/MatrixReflection.java
+++ src/main/java/epi/arrays/multidimensional/_6/_18/variant/MatrixReflection.java
@@ -1,54 +1,195 @@
 package epi.arrays.multidimensional._6._18.variant;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-//Page 92 Variant
-//Time complexity O(n^2)
-//Space complexity O(1)
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Question:
+ * Implement an algorithm to reflect A, assumed to be an n X n 2D array,
+ * about the horizontal axis of symmetry. Repeat the same for reflections about
+ * the vertical axis, the diagonal from top-left to bottom-right, and the diagonal
+ * from top-right to bottom-left.
+ * ---
+ * Solution:
+ * Iterate matrix by rows / columns until we reach the middle axis, performing necessary swaps.
+ * ---
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(1)
+ */
 public class MatrixReflection {
+
     public static void main(String[] args) {
-        int[][] matrix1 = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        int n = matrix1.length;
-        for(int i = 0; i < n / 2; i++) {
-            for(int j = 0; j < n; j++) {
-                int temp = matrix1[i][j];
-                matrix1[i][j] = matrix1[n - 1 - i][j];
-                matrix1[n - 1 - i][j] = temp;
-            }
-        }
-        System.out.println("Horizontal reflection: " + Arrays.deepToString(matrix1));
+        List<List<Integer>> matrix3x3 = Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(4, 5, 6),
+                Arrays.asList(7, 8, 9));
 
-        int[][] matrix2 = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        n = matrix2.length;
-        for(int j = 0; j < n / 2; j++) {
-            for(int i = 0; i < n; i++) {
-                int temp = matrix2[i][j];
-                matrix2[i][j] = matrix2[i][n - 1 - j];
-                matrix2[i][n - 1 - j] = temp;
-            }
-        }
-        System.out.println("Vertical reflection: " + Arrays.deepToString(matrix2));
+        assertEquals(Arrays.asList(
+                Arrays.asList(7, 8, 9),
+                Arrays.asList(4, 5, 6),
+                Arrays.asList(1, 2, 3)
+        ), horizontalReflection(matrix3x3));
 
-        int[][] matrix3 = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        n = matrix3.length;
-        for(int i = 0; i < n - 1; i++) {
-            for(int j = i + 1; j < n; j++) {
-                int temp = matrix3[i][j];
-                matrix3[i][j] = matrix3[j][i];
-                matrix3[j][i] = temp;
-            }
-        }
-        System.out.println("Diagonal from top-left to bottom-right reflection: " + Arrays.deepToString(matrix3));
+        assertEquals(Arrays.asList(
+                Arrays.asList(3, 2, 1),
+                Arrays.asList(6, 5, 4),
+                Arrays.asList(9, 8, 7)
+        ), verticalReflection(matrix3x3));
 
-        int[][] matrix4 = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        n = matrix4.length;
-        for(int i = 0; i < n - 1; i++) {
-            for(int j = 0; j < n - i - 1; j++) {
-                int temp = matrix4[i][j];
-                matrix4[i][j] = matrix4[n - 1 - j][n - 1 - i];
-                matrix4[n - 1 - j][n - 1 - i] = temp;
+        assertEquals(Arrays.asList(
+                Arrays.asList(9, 6, 3),
+                Arrays.asList(8, 5, 2),
+                Arrays.asList(7, 4, 1)
+        ), diagonalTopLeftBottomRightReflection(matrix3x3));
+
+        assertEquals(Arrays.asList(
+                Arrays.asList(1, 4, 7),
+                Arrays.asList(2, 5, 8),
+                Arrays.asList(3, 6, 9)
+        ), diagonalTopRightBottomLeftReflection(matrix3x3));
+
+        List<List<Integer>> matrix4x4 = Arrays.asList(
+                Arrays.asList(1, 2, 3, 4),
+                Arrays.asList(5, 6, 7, 8),
+                Arrays.asList(9, 10, 11, 12),
+                Arrays.asList(13, 14, 15, 16));
+
+        assertEquals(Arrays.asList(
+                Arrays.asList(13, 14, 15, 16),
+                Arrays.asList(9, 10, 11, 12),
+                Arrays.asList(5, 6, 7, 8),
+                Arrays.asList(1, 2, 3, 4)
+        ), horizontalReflection(matrix4x4));
+
+        assertEquals(Arrays.asList(
+                Arrays.asList(4, 3, 2, 1),
+                Arrays.asList(8, 7, 6, 5),
+                Arrays.asList(12, 11, 10, 9),
+                Arrays.asList(16, 15, 14, 13)
+        ), verticalReflection(matrix4x4));
+
+        assertEquals(Arrays.asList(
+                Arrays.asList(16, 12, 8, 4),
+                Arrays.asList(15, 11, 7, 3),
+                Arrays.asList(14, 10, 6, 2),
+                Arrays.asList(13, 9, 5, 1)
+        ), diagonalTopLeftBottomRightReflection(matrix4x4));
+
+        assertEquals(Arrays.asList(
+                Arrays.asList(1, 5, 9, 13),
+                Arrays.asList(2, 6, 10, 14),
+                Arrays.asList(3, 7, 11, 15),
+                Arrays.asList(4, 8, 12, 16)
+        ), diagonalTopRightBottomLeftReflection(matrix4x4));
+
+        List<List<Integer>> matrix5x5 = Arrays.asList(
+                Arrays.asList(1, 2, 3, 4, 5),
+                Arrays.asList(6, 7, 8, 9, 10),
+                Arrays.asList(11, 12, 13, 14, 15),
+                Arrays.asList(16, 17, 18, 19, 20),
+                Arrays.asList(21, 22, 23, 24, 25));
+
+        assertEquals(Arrays.asList(
+                Arrays.asList(21, 22, 23, 24, 25),
+                Arrays.asList(16, 17, 18, 19, 20),
+                Arrays.asList(11, 12, 13, 14, 15),
+                Arrays.asList(6, 7, 8, 9, 10),
+                Arrays.asList(1, 2, 3, 4, 5)
+        ), horizontalReflection(matrix5x5));
+
+        assertEquals(Arrays.asList(
+                Arrays.asList(5, 4, 3, 2, 1),
+                Arrays.asList(10, 9, 8, 7, 6),
+                Arrays.asList(15, 14, 13, 12, 11),
+                Arrays.asList(20, 19, 18, 17, 16),
+                Arrays.asList(25, 24, 23, 22, 21)
+        ), verticalReflection(matrix5x5));
+
+        assertEquals(Arrays.asList(
+                Arrays.asList(25, 20, 15, 10, 5),
+                Arrays.asList(24, 19, 14, 9, 4),
+                Arrays.asList(23, 18, 13, 8, 3),
+                Arrays.asList(22, 17, 12, 7, 2),
+                Arrays.asList(21, 16, 11, 6, 1)
+        ), diagonalTopLeftBottomRightReflection(matrix5x5));
+
+        assertEquals(Arrays.asList(
+                Arrays.asList(1, 6, 11, 16, 21),
+                Arrays.asList(2, 7, 12, 17, 22),
+                Arrays.asList(3, 8, 13, 18, 23),
+                Arrays.asList(4, 9, 14, 19, 24),
+                Arrays.asList(5, 10, 15, 20, 25)
+        ), diagonalTopRightBottomLeftReflection(matrix5x5));
+    }
+
+    private static List<List<Integer>> horizontalReflection(List<List<Integer>> inputSquareMatrix) {
+        List<List<Integer>> squareMatrix = copyList(inputSquareMatrix);
+        int size = squareMatrix.size();
+
+        for (int i = 0; i < size / 2; i++) {
+            for (int j = 0; j < size; j++) {
+                int temp = squareMatrix.get(i).get(j);
+                squareMatrix.get(i).set(j, squareMatrix.get(size - 1 - i).get(j));
+                squareMatrix.get(size - 1 - i).set(j, temp);
             }
         }
-        System.out.println("Diagonal from top-right to bottom-left reflection: " + Arrays.deepToString(matrix4));
+
+        return squareMatrix;
+    }
+
+    private static List<List<Integer>> verticalReflection(List<List<Integer>> inputSquareMatrix) {
+        List<List<Integer>> squareMatrix = copyList(inputSquareMatrix);
+        int size = squareMatrix.size();
+
+        for (int j = 0; j < size / 2; j++) {
+            for (int i = 0; i < size; i++) {
+                int temp = squareMatrix.get(i).get(j);
+                squareMatrix.get(i).set(j, squareMatrix.get(i).get(size - 1 - j));
+                squareMatrix.get(i).set(size - 1 - j, temp);
+            }
+        }
+
+        return squareMatrix;
+    }
+
+    private static List<List<Integer>> diagonalTopLeftBottomRightReflection(List<List<Integer>> inputSquareMatrix) {
+        List<List<Integer>> squareMatrix = copyList(inputSquareMatrix);
+        int size = squareMatrix.size();
+
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - 1 - i; j++) {
+                int temp = squareMatrix.get(i).get(j);
+                squareMatrix.get(i).set(j, squareMatrix.get(size - 1 - j).get(size - 1 - i));
+                squareMatrix.get(size - 1 - j).set(size - 1 - i, temp);
+            }
+        }
+
+        return squareMatrix;
+    }
+
+    private static List<List<Integer>> diagonalTopRightBottomLeftReflection(List<List<Integer>> inputSquareMatrix) {
+        List<List<Integer>> squareMatrix = copyList(inputSquareMatrix);
+        int size = squareMatrix.size();
+
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = size - 1; j > i; j--) {
+                int temp = squareMatrix.get(i).get(j);
+                squareMatrix.get(i).set(j, squareMatrix.get(j).get(i));
+                squareMatrix.get(j).set(i, temp);
+            }
+        }
+
+        return squareMatrix;
+    }
+
+    private static List<List<Integer>> copyList(List<List<Integer>> input) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (List<Integer> item : input) {
+            result.add(new ArrayList<>(item));
+        }
+        return result;
     }
 }
